424e8dcc848a7f74c1d961fd06500449c10a27e3
aybe <aybe@users.noreply.github.com>
2023-12-24 22:44:01 +0100

    read header

--- libpsn00b/psxpress.tests/MDEC/UnitTest4.cs
+++ libpsn00b/psxpress.tests/MDEC/UnitTest4.cs
@@ -2,6 +2,9 @@
 // ReSharper disable InconsistentNaming
 // ReSharper disable StringLiteralTypo
 
+using System.Globalization;
+using System.Numerics;
+
 namespace psxpress.tests.MDEC;
 
 [TestClass]
@@ -114,5 +117,42 @@ public sealed class UnitTest4 : UnitTestBase
 	private void Decode(KeyValuePair<uint, FrameData> frameData)
 	{
 		using var stream = new MemoryStream(frameData.Value.Data);
+		using var reader = new BitStreamReader(stream, BitStreamFormat.ShortLittleEndianMostSignificant);
+
+		var mdecSizeBy4 = stream.Read<ushort>();
+		var mdecFileId = stream.Read<ushort>();
+		var quantStep = stream.Read<ushort>();
+		var version = stream.Read<ushort>();
+
+		WriteLine($"{nameof(mdecSizeBy4)}: {mdecSizeBy4}");
+		WriteLine($"{nameof(mdecFileId)}: 0x{mdecFileId:X4}");
+		WriteLine($"{nameof(quantStep)}: {quantStep}");
+		WriteLine($"{nameof(version)}: {version}");
+
+		Assert.AreEqual(0x08C0, mdecSizeBy4);
+		Assert.AreEqual(0x3800, mdecFileId);
+		Assert.AreEqual(0x0001, quantStep);
+		Assert.AreEqual(0x0001, version);
+		
+		WriteLine();
+
+		WriteLine($"0x{(ushort)0x20FE:X4} = {ToBinaryString((ushort)0x20FE)}");
+		WriteLine($"0x{(ushort)0xFE20:X4} = {ToBinaryString((ushort)0xFE20)}");
+
+		while (stream.Position < stream.Length)
+		{
+			var dc = reader.Read<short>(10);
+			WriteLine($"{nameof(dc)}:      {ToBinaryString(dc)} {dc}");
+			break;
+		}
+	}
+
+	private static string ToBinaryString<T>(T value) where T : IBinaryInteger<T>
+	{
+		var s = value.ToString($"B{value.GetByteCount() * 8}", CultureInfo.InvariantCulture);
+		var chunk = s.Chunk(8);
+		s = string.Join("_", chunk.Select(chars => new string(chars)));
+		return s;
+		return s;
 	}
 }
