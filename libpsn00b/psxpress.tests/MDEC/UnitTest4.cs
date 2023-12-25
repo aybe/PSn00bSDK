d2d92e0ef640fcb0ddee17c16b14a334db7dc5f7
aybe <aybe@users.noreply.github.com>
2023-12-25 01:09:51 +0100

    whatever...

--- libpsn00b/psxpress.tests/MDEC/UnitTest4.cs
+++ libpsn00b/psxpress.tests/MDEC/UnitTest4.cs
@@ -55,7 +55,7 @@ public sealed class UnitTest4 : UnitTestBase
 
 			if (!subHeader1.SubMode.HasFlag(SectorSubMode.Video))
 			{
-				continue;
+				break;
 			}
 
 			Assert.AreEqual(128, subHeader1.CodingInformation);
@@ -116,6 +116,19 @@ public sealed class UnitTest4 : UnitTestBase
 
 	private void Decode(KeyValuePair<uint, FrameData> frameData)
 	{
+
+		WriteLine($"0x{0xFE20:X4}, {ToBinaryString((ushort)0xFE20)}");
+		WriteLine($"0x{0x2291:X4}, {ToBinaryString((ushort)0x2291)}");
+		WriteLine($"0x{0xF1F4:X4}, {ToBinaryString((ushort)0xF1F4)}");
+		WriteLine($"0x{0x8948:X4}, {ToBinaryString((ushort)0x8948)}");
+		WriteLine($"0x{0xBB35:X4}, {ToBinaryString((ushort)0xBB35)}");
+		WriteLine($"0x{0x210C:X4}, {ToBinaryString((ushort)0x210C)}");
+
+		var bytes = new byte[] { 0xFE, 0x20, 0x22, 0x91, 0xF1, 0xF4, 0x89, 0x48, 0xBB, 0x35, 0x21, 0x0C };
+		foreach (var b in bytes)
+		{
+			WriteLine($"0x{b:X2} {ToBinaryString(b)}");
+		}
 		using var stream = new MemoryStream(frameData.Value.Data);
 		using var reader = new BitStreamReader(stream, BitStreamFormat.ShortLittleEndianMostSignificant);
 
@@ -145,6 +158,130 @@ public sealed class UnitTest4 : UnitTestBase
 			var eb = reader.Read<ushort>(2);
 			Assert.AreEqual(2, eb);
 
+			if (reader.Peek<ushort>(2) == 0b_10)
+			{
+				reader.Read<ushort>(2);
+				break; // EOB
+			}
+
+			if (reader.Peek<ushort>(2) == 0b_11)
+			{
+				reader.Read<ushort>(2);
+				var s = reader.Read<ushort>(1);
+				break;
+			}
+
+			if (reader.Peek<ushort>(3) == 0b_011)
+			{
+				reader.Read<ushort>(3);
+				var s = reader.Read<ushort>(1);
+				break;
+			}
+
+			if (reader.Peek<ushort>(3) == 0b_010)
+			{
+				reader.Read<ushort>(3);
+				var x = reader.Read<ushort>(1);
+				var s = reader.Read<ushort>(1);
+				break;
+			}
+
+			if (reader.Peek<ushort>(4) == 0b_0011)
+			{
+				reader.Read<ushort>(4);
+				var x = reader.Read<ushort>(1);
+				var s = reader.Read<ushort>(1);
+				break;
+			}
+
+			if (reader.Peek<ushort>(5) == 0b_0_0101)
+			{
+				reader.Read<ushort>(5);
+				var s = reader.Read<ushort>(1);
+				break;
+			}
+
+			if (reader.Peek<ushort>(5) == 0b_0_0100)
+			{
+				reader.Read<ushort>(5);
+				var x = reader.Read<ushort>(3);
+				var s = reader.Read<ushort>(1);
+				break;
+			}
+
+			if (reader.Peek<ushort>(4) == 0b_0001)
+			{
+				reader.Read<ushort>(4);
+				var x = reader.Read<ushort>(2);
+				var s = reader.Read<ushort>(1);
+				break;
+			}
+
+			if (reader.Peek<ushort>(5) == 0b_0_0001)
+			{
+				reader.Read<ushort>(5);
+				var x = reader.Read<ushort>(2);
+				var s = reader.Read<ushort>(1);
+				break;
+			}
+
+			if (reader.Peek<ushort>(6) == 0b_00_0001)
+			{
+				reader.Read<ushort>(6);
+				var x = reader.Read<ushort>(10);
+				break; // escape
+			}
+
+			if (reader.Peek<ushort>(7) == 0b_000_0001)
+			{
+				reader.Read<ushort>(7);
+				var x = reader.Read<ushort>(3);
+				var s = reader.Read<ushort>(1);
+				WriteLine(x);
+				WriteLine(s);
+				break;
+			}
+
+
+			if (reader.Peek<ushort>(8) == 0b_0000_0001)
+			{
+				reader.Read<ushort>(8);
+				var x = reader.Read<ushort>(4);
+				var s = reader.Read<ushort>(1);
+				break;
+			}
+
+			if (reader.Peek<ushort>(9) == 0b_0_0000_0001)
+			{
+				reader.Read<ushort>(9);
+				var x = reader.Read<ushort>(4);
+				var s = reader.Read<ushort>(1);
+				break;
+			}
+
+			if (reader.Peek<ushort>(10) == 0b_00_0000_0001)
+			{
+				reader.Read<ushort>(10);
+				var x = reader.Read<ushort>(4);
+				var s = reader.Read<ushort>(1);
+				break;
+			}
+
+			if (reader.Peek<ushort>(11) == 0b_000_0000_0001)
+			{
+				reader.Read<ushort>(11);
+				var x = reader.Read<ushort>(4);
+				var s = reader.Read<ushort>(1);
+				break;
+			}
+
+			if (reader.Peek<ushort>(12) == 0b_0000_0000_0001)
+			{
+				reader.Read<ushort>(12);
+				var x = reader.Read<ushort>(4);
+				var s = reader.Read<ushort>(1);
+			}
+
 			break;
 		}
 	}
@@ -152,7 +289,7 @@ public sealed class UnitTest4 : UnitTestBase
 	private static string ToBinaryString<T>(T value) where T : IBinaryInteger<T>
 	{
 		var s = value.ToString($"B{value.GetByteCount() * 8}", CultureInfo.InvariantCulture);
-		var chunk = s.Chunk(8);
+		var chunk = s.Chunk(4);
 		s = string.Join("_", chunk.Select(chars => new string(chars)));
 		return s;
 		return s;
